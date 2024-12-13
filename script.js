75fba9cb5407245cf916febed75bbf8a02a7ee0a
durdonax <durdonasheykhadx@gmail.com>
2024-12-13 16:27:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let ism = prompt(`Ism kiriting`)
+let fam = prompt(`Familiya kiriting`)
+let yosh = prompt(`Yosh kiriting`)
+let shahar = prompt(`Shahar kiriting`)
+
+const user = {
+    name:ism,
+    surname:fam,
+    age:yosh,
+    city:shahar,
+}
+console.log(user)
+
+
+
+
